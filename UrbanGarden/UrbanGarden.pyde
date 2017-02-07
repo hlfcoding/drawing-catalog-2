d758c4aa1e9b94b9e8c0572ca18e27e74056f00c
Peng Wang <peng@pengxwang.com>
2017-02-06 23:59:55 -0800

    UrbanGarden: Initial umbrella coloring via names.

--- UrbanGarden/UrbanGarden.pyde
+++ UrbanGarden/UrbanGarden.pyde
@@ -1,7 +1,10 @@
 from umbrella import *
 
+colorMode(HSB, 1)
 shapeMode(CENTER)
 translate(width / 2, height / 2)
+noStroke()
 
 u = Umbrella()
+u.setColor(0, 0.4, 1)
 shape(u.shape)
--- UrbanGarden/umbrella.py
+++ UrbanGarden/umbrella.py
@@ -9,18 +9,28 @@ class Umbrella(object):
     def diameter(self):
         return self.radius * 2
 
+    def setColor(self, h, s, b):
+        self.shape.getChild('canopy').setFill(color(h, s, b, 0.9))
+        rC = color(h, s, b - 0.1)
+        for r in self.shape.getChild('ribs').getChildren():
+            r.setFill(rC)
+
     def createShape(self):
         s = createShape(GROUP)
 
         canopy = createShape(ELLIPSE, 0, 0, self.diameter, self.diameter)
         s.addChild(canopy)
+        s.addName('canopy', canopy)
 
+        ribs = createShape(GROUP)
         for i in range(0, self.ribCount):
             offset = 0.2
             h = self.diameter / 20
-            rib = createShape(RECT, -self.radius * (1 + offset), -h / 2,
-                              self.diameter * (1 + offset), h)
-            rib.rotate(PI * i / self.ribCount)
-            s.addChild(rib)
+            r = createShape(RECT, -self.radius * (1 + offset), -h / 2,
+                            self.diameter * (1 + offset), h)
+            r.rotate(PI * i / self.ribCount)
+            ribs.addChild(r)
+        s.addChild(ribs)
+        s.addName('ribs', ribs)
 
         return s
