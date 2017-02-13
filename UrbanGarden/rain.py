610f04f5da5032f4afd6ef3f3319b0b65994a2f0
Peng Wang <peng@pengxwang.com>
2017-02-12 19:44:54 -0800

    UrbanGarden: Initial rain implementation via random.

--- UrbanGarden/rain.py
+++ UrbanGarden/rain.py
@@ -2,13 +2,20 @@ from hlf.core import SubSketch
 
 class Rain(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, count, bounds):
+        xmin, ymin, xmax, ymax = bounds
+        r = range(0, count)
+        self.positions = [(random(xmin, xmax), random(ymin, ymax)) for i in r]
+        self.lengths = [random(1, 5) for i in r]
 
 class RainTest(SubSketch):
 
     def setup(self):
-        self.subject = Rain()
+        stroke(0)
+        self.isCentered = False
+        self.subject = Rain(count=50, bounds=(0, 0, 100, 100))
 
     def draw(self):
-        pass
+        for i, (x, y) in enumerate(self.subject.positions):
+            l = self.subject.lengths[i]
+            line(x, y, x, y + l)
