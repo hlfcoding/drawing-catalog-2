9b9f7d6c00231f3a878111dbd25913715434d05d
Peng Wang <peng@pengxwang.com>
2017-02-13 23:56:53 -0800

    UrbanGarden: Refine rain noise via jittered-grid.
    Includes refactor.

--- UrbanGarden/rain.py
+++ UrbanGarden/rain.py
@@ -3,18 +3,31 @@ from hlf.core import SubSketch
 class Rain(object):
 
     def __init__(self, count, bounds):
-        x, y, w, h = bounds
+        self.bounds = bounds
+        self.count = count
+        self.positions = []
+        self.distribute()
         r = range(0, count)
-        c = float(count)
-        self.positions = [(noise(i, 0) * w, noise(0, i) * h) for i in r]
-        self.lengths = [1 + noise(i) * 4 for i in r]
+        self.lengths = [1 + noise(i) * 2 for i in r]
+    
+    def distribute(self):
+        ox, oy, w, h = self.bounds
+        gridSize = int(sqrt(self.count))
+        cellW = w / gridSize
+        cellH = h / gridSize
+        r = range(0, gridSize)
+        for i in r:
+            for j in r:
+                x = ox + (i * cellW) + (noise(i, j) * -2 + 1) * cellW
+                y = oy + (j * cellH) + (noise(j, i) * -2 + 1) * cellH
+                self.positions.append((x, y))
 
 class RainTest(SubSketch):
 
     def setup(self):
         stroke(0)
         self.isCentered = False
-        self.subject = Rain(count=100, bounds=(0, 0, 100, 100))
+        self.subject = Rain(count=100, bounds=(0.0, 0.0, 100.0, 100.0))
 
     def draw(self):
         for i, (x, y) in enumerate(self.subject.positions):
